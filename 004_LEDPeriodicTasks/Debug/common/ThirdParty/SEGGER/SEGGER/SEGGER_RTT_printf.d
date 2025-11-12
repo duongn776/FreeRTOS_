57549514cef0c7ab3444d548cdf58c0d3f1e8d83
duongn776 <duongn776@gmail.com>
2025-11-12 11:28:27 +0700

    004-led in periodic state

---
+++ 004_LEDPeriodicTasks/Debug/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT_printf.d
@@ -0,0 +1,6 @@
+common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT_printf.o: \
+ D:/RTOS/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT_printf.c \
+ D:/RTOS/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
+ D:/RTOS/RTOS_workspace/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
+D:/RTOS/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
+D:/RTOS/RTOS_workspace/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
