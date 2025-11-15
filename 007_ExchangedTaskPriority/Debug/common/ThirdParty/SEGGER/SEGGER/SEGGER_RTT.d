8f017c3b1f88c050d12447f07b23172af7f499c0
duongn776 <duongn776@gmail.com>
2025-11-15 15:17:40 +0700

    006 and 007 for btn isr and exchanged priority

---
+++ 007_ExchangedTaskPriority/Debug/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.d
@@ -0,0 +1,6 @@
+common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.o: \
+ D:/RTOS/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.c \
+ D:/RTOS/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
+ D:/RTOS/RTOS_workspace/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
+D:/RTOS/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
+D:/RTOS/RTOS_workspace/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
