ff9eaacf631bdae3f5b8a8a756ad555ef6c50468
duongn776 <duongn776@gmail.com>
2025-11-16 21:54:44 +0700

    implement binary semaphore between 2 tasks

---
+++ 009_BinarySemaphore/Debug/common/ThirdParty/FreeRTOS/list.d
@@ -0,0 +1,18 @@
+common/ThirdParty/FreeRTOS/list.o: \
+ D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/list.c \
+ D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ ../Core/Inc/FreeRTOSConfig.h \
+ D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/include/projdefs.h \
+ D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/include/portable.h \
+ D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/include/list.h
+D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+../Core/Inc/FreeRTOSConfig.h:
+D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/include/projdefs.h:
+D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/include/portable.h:
+D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/include/list.h:
---
+++ 009_BinarySemaphore/Debug/common/ThirdParty/FreeRTOS/portable/MemMang/heap_4.d
@@ -0,0 +1,20 @@
+common/ThirdParty/FreeRTOS/portable/MemMang/heap_4.o: \
+ D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/portable/MemMang/heap_4.c \
+ D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ ../Core/Inc/FreeRTOSConfig.h \
+ D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/include/projdefs.h \
+ D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/include/portable.h \
+ D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/include/task.h \
+ D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/include/list.h
+D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+../Core/Inc/FreeRTOSConfig.h:
+D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/include/projdefs.h:
+D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/include/portable.h:
+D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/include/task.h:
+D:/RTOS/RTOS_workspace/common/ThirdParty/FreeRTOS/include/list.h:
